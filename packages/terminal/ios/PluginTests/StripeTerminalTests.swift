b5b31819539c49a7aa18a0fb8efd4e01b45b0688
Shiva Prasad <sp@shiv19.com>
2025-05-28 16:12:46 +1200

    fix ios plugin build

--- packages/terminal/ios/PluginTests/StripeTerminalTests.swift
+++ packages/terminal/ios/PluginTests/StripeTerminalTests.swift
@@ -1,15 +1,16 @@
 import XCTest
-@testable import StripeTerminalPlugin
+@testable import Plugin
 
 class StripeTerminalTests: XCTestCase {
+
     func testEcho() {
         // This is an example of a functional test case for a plugin.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
 
         let implementation = StripeTerminal()
-        //         let value = "Hello, World!"
-        //         let result = implementation.echo(value)
-        //
-        //         XCTAssertEqual(value, result)
+        let value = "Hello, World!"
+        let result = implementation.echo(value)
+
+        XCTAssertEqual(value, result)
     }
 }
